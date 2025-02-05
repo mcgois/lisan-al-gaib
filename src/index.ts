8e21e1441b8c8c6d1b0c15c65a2d15ed0c4f8dab
Matheus Cabral <matheus.cabral@gmail.com>
2025-02-05 20:14:18 -0300

    feat: :sparkles: teste3

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,7 @@ export function teste(a: number, b: number) {
 export function teste2(a: number, b: number) {
   return a - b;
 }
+
+export function teste3(a: number, b: number) {
+  return a * b;
+}
