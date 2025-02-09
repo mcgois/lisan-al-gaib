a0e420c24d97c070b6e49bc99512aeb9f9e3bfa7
Matheus Cabral <matheus.cabral@gmail.com>
2025-02-09 18:43:33 -0300

    refactor: :recycle: refactoring.
    Refactoring.

--- src/init.ts
+++ src/init.ts
@@ -1,23 +1,23 @@
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 import yaml from 'js-yaml';
 import ora from 'ora';
 
 import configuration from './configuration';
 
-export function init() {
+export async function init() {
   const spinner = ora('Creating lisan-al-gaib configuration file').start();
 
   const yamlContent = yaml.dump(configuration, {
     sortKeys: true,
   });
 
-  fs.writeFile('.lisan-al-gaib.yml', yamlContent, (err) => {
-    if (err) {
-      spinner.fail('Failed to create lisan-al-gaib configuration');
-    } else {
-      spinner.succeed('lisan-al-gaib configuration created');
-    }
+  try {
+    await fs.writeFile('.lisan-al-gaib.yml', yamlContent);
+    spinner.succeed('lisan-al-gaib configuration created');
+  } catch (err) {
+    spinner.fail('Failed to create lisan-al-gaib configuration');
+    return;
+  }
 
-    spinner.stop();
-  });
+  spinner.stop();
 }
